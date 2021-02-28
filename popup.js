976f3558718697d8959db6fe9cd701c65567bba4
Akash Prasad <akash.akpr@gmail.com>
2021-02-27 20:50:55 -0800

    updated popup.js and .html

--- popup.js
+++ popup.js
@@ -1,18 +1,22 @@
 var sceneData = {};
-var test = 0;
 
 $(function(){
   $('#sentSceneData').click(function(){
-    chrome.storage.sync.get(['time','songName'],function(sceneInfo){
-       sceneData[$('#time').val()] = $('#songName').val();
+    chrome.storage.sync.get(['minutes','seconds','songName'],function(sceneInfo){
+      if($('#minutes').val() && $('#seconds').val() && $("#songName").val()){
+      var time = parseInt($("#minutes").val() * 60) + parseInt($('#seconds').val());
+      sceneData[time] = $('#songName').val();
       for (var key in sceneData){
         chrome.extension.getBackgroundPage().console.log(key + ":" + sceneData[key]);
       };
-      $('#time').val('');
+      $('#minutes').val('');
+      $('#seconds').val('');
       $('#songName').val('');
 
-
-
+    }
+      else{
+        alert("You have not inputted valid values");
+      }
     })
   })
 });
